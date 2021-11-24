1315e0a5948473a104919b6bdfa2e8f226de5eb0
xdrlv21 <93651556+xdrlv21@users.noreply.github.com>
2021-11-24 22:03:58 +0700

    upload file web
    upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const txtElement = ['Ordinary Student', 'Gamer', 'Cheater', 'boy who likes to do new things'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+    if(count == txtElement.length){
+        count = 0;
+    }
+
+    currentTxt = txtElement[count];
+
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek-ngetik').textContent = words;
+
+    if(words.length == currentTxt.length){
+        count++;
+        txtIndex =0;
+    }
+
+    setTimeout(ngetik, 300);
+})();
